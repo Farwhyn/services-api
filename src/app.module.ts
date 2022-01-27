2214acbe73a9d24cd5841ea22aa9adb2497205da
Farwhyn <jamesizhou@gmail.com>
2022-01-26 23:49:03 -0800

    added services routes and entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServiceModule } from './service/service.module';
+import config from 'config';
+import { Service } from './service/service.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: config.get('db_type'),
+      host: config.get('db_host'),
+      port: config.get('db_port'),
+      username: config.get('db_username'),
+      password: config.get('db_password'),
+      database: config.get('db_name'),
+      entities: [Service],
+      synchronize: true,
+    }),
+    ServiceModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
